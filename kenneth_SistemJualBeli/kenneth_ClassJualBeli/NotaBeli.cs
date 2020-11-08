300d9d80746503dab6da7ec1ac50732b73506e25
Kenneth Manuel <kennethlieyanto99@gmail.com>
2020-11-08 15:20:36 +0700

    Week 10 Assignment

--- kenneth_SistemJualBeli/kenneth_ClassJualBeli/NotaBeli.cs
+++ kenneth_SistemJualBeli/kenneth_ClassJualBeli/NotaBeli.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MySql.Data.MySqlClient;
 
 namespace kenneth_ClassJualBeli
 {
@@ -9,32 +10,89 @@ namespace kenneth_ClassJualBeli
     {
         private string noNota;
         private DateTime tanggal;
+        private Supplier supplier;
+        private Pegawai pegawai;
+        private List<NotaBeliDetil> listNotaBeliDetil;
 
-        public Pegawai Pegawai
+        #region Constructors
+        public NotaBeli(string noNota, DateTime tanggal, Supplier supplier, Pegawai pegawai)
         {
-            get => default;
-            set
-            {
-            }
+            this.NoNota = noNota;
+            this.Tanggal = tanggal;
+            this.Supplier = supplier;
+            this.Pegawai = pegawai;
+            this.ListNotaBeliDetil = listNotaBeliDetil;
         }
+        #endregion
 
-        public Supplier Supplier
-        {
-            get => default;
-            set
-            {
-            }
-        }
-
-        public NotaBeliDetil NotaBeliDetil
-        {
-            get => default;
-            set
-            {
-            }
-        }
-
+        #region Properties
         public string NoNota { get => noNota; set => noNota = value; }
         public DateTime Tanggal { get => tanggal; set => tanggal = value; }
+        public Supplier Supplier { get => supplier; set => supplier = value; }
+        public Pegawai Pegawai { get => pegawai; set => pegawai = value; }
+        public List<NotaBeliDetil> ListNotaBeliDetil { get => listNotaBeliDetil; private set => listNotaBeliDetil = value; }
+        #endregion
+
+        #region Methods
+        // Menambahkan detil barang dalam nota
+        public void TambahNotaBeliDetil(int harga, int jumlah, Barang barang)
+        {
+            NotaBeliDetil notaBeliDetil = new NotaBeliDetil(harga, jumlah, barang);
+
+            listNotaBeliDetil.Add(notaBeliDetil);
+        }
+
+        // Menambah nota beli baru
+        public static void TambahData(NotaBeli nota)
+        {
+            string sql1 = "INSERT INTO notabeli(nonota, tanggal, kodesupplier, kodepegawai) VALUES ('" + nota.NoNota + "','" + nota.Tanggal.ToString("yyyy-MM-dd hh:mm:ss") + "','" + nota.Supplier.KodeSupplier + "','" + nota.Pegawai.KodePegawai + "')";
+
+            Koneksi.JalankanPerintahDML(sql1);
+
+            // Mendapatkan semua detil barang yang ada di nota beli
+            foreach (NotaBeliDetil detilNota in nota.ListNotaBeliDetil)
+            {
+                string sql2 = "INSERT INTO notabelidetil(nonota, kodebarang, harga, jumlah) VALUES ('" + nota.NoNota + "','" + detilNota.Barang.KodeBarang + "','" + detilNota.Harga + "','" + detilNota.Jumlah + "')";
+
+                Koneksi.JalankanPerintahDML(sql2);
+
+                // Update stok barang dibeli
+                Barang.UpdateStok("pembelian", detilNota.Barang.KodeBarang, detilNota.Jumlah);
+            }
+        }
+
+        // Generate nomor nota
+        public static string GenerateNoNota()
+        {
+            string sql = "SELECT RIGHT(nonota, 3) AS nouruttransaksi " +
+                         "FROM notabeli " +
+                         "WHERE DATE(tanggal) = DATE(CURRENT_DATE) " +
+                         "ORDER BY tanggal DESC LIMIT 1";
+
+            MySqlDataReader hasil = Koneksi.JalankanPerintahQuery(sql);
+            string hasilNoNota = "";
+            if(hasil.Read() == true)
+            {
+                if(hasil.GetValue(0).ToString() != "")
+                {
+                    int noUrutTrans = int.Parse(hasil.GetValue(0).ToString()) + 1;
+                    hasilNoNota = DateTime.Now.Year +
+                                  DateTime.Now.Month.ToString().PadLeft(2, '0') +
+                                  DateTime.Now.Day.ToString().PadLeft(2, '0') +
+                                  noUrutTrans.ToString().PadLeft(3, '0');
+                }
+            }
+            else
+            {
+                hasilNoNota = DateTime.Now.Year +
+                              DateTime.Now.Month.ToString().PadLeft(2, '0') +
+                              DateTime.Now.Day.ToString().PadLeft(2, '0') +
+                              "001";
+            }
+            return hasilNoNota;
+        } 
+        #endregion
+
+
     }
 }
--- kenneth_SistemJualBeli/kenneth_ClassJualBeli/NotaBeliDetil.cs
+++ kenneth_SistemJualBeli/kenneth_ClassJualBeli/NotaBeliDetil.cs
@@ -9,16 +9,25 @@ namespace kenneth_ClassJualBeli
     {
         private int harga;
         private int jumlah;
+        private Barang barang;
 
-        public Barang Barang
+        #region Constructors
+        public NotaBeliDetil(int harga, int jumlah, Barang barang)
         {
-            get => default;
-            set
-            {
-            }
+            this.Harga = harga;
+            this.Jumlah = jumlah;
+            this.Barang = barang;
         }
+        #endregion
 
+        #region Properties
         public int Harga { get => harga; set => harga = value; }
         public int Jumlah { get => jumlah; set => jumlah = value; }
+        public Barang Barang { get => barang; set => barang = value; }
+        #endregion
+
+        #region Methods
+
+        #endregion
     }
 }
